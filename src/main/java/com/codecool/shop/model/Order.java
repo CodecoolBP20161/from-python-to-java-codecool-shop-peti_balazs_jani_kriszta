3933da822effb70284dc091c4069e3d553ea0fca
Komlancz Janos <jkomlancz@gmail.com>
2016-11-09 14:05:27 +0100

    order class is created

---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.model;
+
+import java.util.List;
+
+public class Order {
+    private int id;
+    private List lineItems;
+
+    public Order(List shoppingCartItems){
+        this.lineItems = shoppingCartItems;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List getLineItems() {
+        return lineItems;
+    }
+}
