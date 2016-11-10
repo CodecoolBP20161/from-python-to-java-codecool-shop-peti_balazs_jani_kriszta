1432bf03cd5685ac64a3196aec618e2925100807
KrisztinaAnna <krisztina.baranyai@gmail.com>
2016-11-10 16:39:33 +0100

    fix getTotalPrice method

--- src/main/java/com/codecool/shop/model/ShoppingCart.java
+++ src/main/java/com/codecool/shop/model/ShoppingCart.java
@@ -21,16 +21,14 @@ public class ShoppingCart {
         if (lineItems.containsKey(newItem.getProductID())) {
             lineItems.get(newItem.getProductID()).setQuantity();
             lineItems.get(newItem.getProductID()).setSubtotal();
-            setTotalPrice();
+            setTotalPrice(newItem.getDefaultPrice());
             setQuantity();
-            setTotalPrice();
 
         } else {
             newItem.setSubtotal(newItem.getDefaultPrice());
             addToMap(newItem);
-            setTotalPrice();
+            setTotalPrice(newItem.getDefaultPrice());
             setQuantity();
-            setTotalPrice();
         }
     }
 
@@ -41,11 +39,10 @@ public class ShoppingCart {
         return totalQuantity;
     }
 
-    public void setTotalPrice() {
-        for (LineItem item : lineItems.values()){
-            totalPrice += item.getSubtotal();
-        }
+    private void setTotalPrice(float price) {
+        totalPrice += price;
     }
+
     public float getTotalPrice() {
         return totalPrice;
     }
