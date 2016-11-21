8160238cbe939c3a5dc556f81d0688c31c5de9ac
Komlancz Janos <jkomlancz@gmail.com>
2016-11-21 15:37:20 +0100

    product_categories, suppliers, products, tables created

---
+++ src/main/resources/sql/init_db.sql
@@ -0,0 +1,30 @@
+-- DROP TABLE IF EXISTS products;
+-- DROP TABLE IF EXISTS product_categories;
+-- DROP TABLE IF EXISTS suppliers;
+
+CREATE TABLE product_categories
+(
+id SERIAL PRIMARY KEY,
+name varchar(40),
+description varchar,
+department VARCHAR
+);
+
+CREATE TABLE suppliers
+(
+id SERIAL PRIMARY KEY,
+name varchar(40),
+description varchar
+);
+CREATE TABLE products
+(
+id SERIAL PRIMARY KEY,
+name varchar(40),
+category_id INTEGER,
+supplier_id INTEGER,
+description varchar,
+default_price FLOAT,
+default_currency FLOAT,
+FOREIGN KEY (category_id) REFERENCES product_categories(id),
+FOREIGN KEY (supplier_id) REFERENCES suppliers(id)
+);
