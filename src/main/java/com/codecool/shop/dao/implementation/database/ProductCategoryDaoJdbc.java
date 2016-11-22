c539804a98f4088572f918f6f10b324722341e34
Komlancz Janos <jkomlancz@gmail.com>
2016-11-22 14:16:50 +0100

    without Connection file

--- src/main/java/com/codecool/shop/dao/implementation/database/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/ProductCategoryDaoJdbc.java
@@ -1,7 +1,100 @@
 package com.codecool.shop.dao.implementation.database;
 
-/**
- * Created by komlancz on 2016.11.22..
- */
-public class ProductCategoryDaoJdbc {
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductCategoryDaoJdbc implements ProductCategoryDao {
+    private String sql;
+    private DBConnection connection = new DBConnection();
+    private Statement stmt;
+
+    @Override
+    public void add(ProductCategory category) {
+        String name = category.getName();
+        String description = category.getDescription();
+        String department = category.getDepartment();
+        int counter = 1;
+        try {
+            category.setId(counter);
+            sql = "INSERT INTO product_categories (name, description, department) VALUES ('" + name + "','" + description + "','" + department + "')";
+            stmt = connection.getConnection().createStatement();
+            stmt.executeUpdate(sql);
+            counter++;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public ProductCategory find(int productID) {
+        try {
+            sql = "SELECT * FROM product_categories WHERE id="+productID+";";
+            stmt = connection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            ProductCategory productCategory;
+            if (rs.next()){
+                return productCategory= new ProductCategory(rs.getString("name"), rs.getString("department"), rs.getString("description"));
+            }
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public void remove(int productID) {
+        try {
+            sql = "DELETE FROM product_categories WHERE id="+productID+";";
+            stmt = connection.getConnection().createStatement();
+            stmt.executeQuery(sql);
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<ProductCategory> getAll() {
+        ProductCategory productCategory;
+        List<ProductCategory> allCategories;
+        try {
+            sql = "SELECT * FROM product_categories;";
+            stmt = connection.getConnection().createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            allCategories = new ArrayList<>();
+            while (rs.next()){
+                productCategory= new ProductCategory(rs.getString("name"),rs.getString("department"),rs.getString("description"));
+                productCategory.setId(rs.getInt("id"));
+                allCategories.add(productCategory);
+            }
+            return allCategories;
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//    private java.sql.Connection getConnection() throws SQLException {
+//        return DriverManager.getConnection(
+//                DATABASE,
+//                DB_USER,
+//                DB_PASSWORD);
+//    }
+//
+//    private void executeQuery(String query) {
+//        try (java.sql.Connection connection = getConnection();
+//             Statement statement = connection.createStatement();
+//        ) {
+//            statement.execute(query);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
--- src/main/java/com/codecool/shop/dao/implementation/database/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/database/ProductDaoJdbc.java
@@ -1,7 +1,60 @@
 package com.codecool.shop.dao.implementation.database;
 
-/**
- * Created by komlancz on 2016.11.22..
- */
-public class ProductDaoJdbc {
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+
+public class ProductDaoJdbc implements ProductDao {
+    private DBConnection connection = new DBConnection();
+    private String sql;
+    private Statement stmt;
+
+    @Override
+    public void add(Product product) {
+        String name = product.getName();
+        int catId = product.getProductCategory().getId();
+        int suppId = product.getSupplier().getId();
+        String description = product.getDescription();
+        float defPrice = product.getDefaultPrice();
+        String defCurr = product.getDefaultCurrency().getDisplayName();
+        try {
+            sql = "INSERT INTO products (name,category_id,supplier_id, description, default_price,default_currency) " +
+                    "VALUES ('"+name+"','"+catId+"','"+suppId+"','"+description+"','"+defPrice+"','"+defCurr+"');";
+            stmt = connection.getConnection().createStatement();
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
 }
