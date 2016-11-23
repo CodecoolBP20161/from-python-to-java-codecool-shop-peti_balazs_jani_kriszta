c23c206fcf1a37ab1d4e0f18723be5f598c96c0b
KrisztinaAnna <krisztina.baranyai@gmail.com>
2016-11-23 13:44:21 +0100

    change instance methods to static methods in Controller

--- src/main/java/com/codecool/shop/controller/Controller.java
+++ src/main/java/com/codecool/shop/controller/Controller.java
@@ -4,17 +4,17 @@ package com.codecool.shop.controller;
  * Created by krisztinabaranyai on 23/11/2016.
  */
 public class Controller {
-    private String state;
+    private static String state;
 
-    public void setState(String state) {
-        this.state = state;
+    public static void setState(String newState) {
+        state = newState;
     }
 
-    public String getState() {
+    public static String getState() {
         return state;
     }
 
-    public void doAct(){
+    public static void doAct(){
         if (state.equals("DB")) {
             ProductControllerJdbc.setAttributes();
         } else if (state.equals("MEM")) {
