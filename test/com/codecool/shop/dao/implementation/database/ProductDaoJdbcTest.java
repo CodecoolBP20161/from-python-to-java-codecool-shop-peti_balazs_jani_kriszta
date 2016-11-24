b55e640b3fc8f6c36d2446dbe8d8c55e9388d74a
ppayter-cc <pomsarpeter@gmail.com>
2016-11-24 14:26:55 +0100

    Before & AfterClass methods implemented

--- test/com/codecool/shop/dao/implementation/database/ProductDaoJdbcTest.java
+++ test/com/codecool/shop/dao/implementation/database/ProductDaoJdbcTest.java
@@ -1,20 +1,63 @@
 package com.codecool.shop.dao.implementation.database;
 
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.PopulateData;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 import static org.junit.Assert.*;
 
+
 public class ProductDaoJdbcTest {
+
+    private ProductDao productDao;
+
+
     @Before
     public void setUp() throws Exception {
+        /*
+        clearing database before populate
+         */
+        String url = "jdbc:postgresql://localhost:5432/codecoolshop";
+        String username = "postgres";
+        String password = "postgres";
 
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement stmt = connection.createStatement();
+
+        // Use TRUNCATE
+        String sql = "TRUNCATE products, product_categories, suppliers";
+        // Execute deletion
+        stmt.executeUpdate(sql);
+
+        // repopulate tables
+        PopulateDataMock.populateDataMock();
+
+        productDao = new ProductDaoJdbc();
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @AfterClass
+    public static void tearDown() throws Exception {
+        /*
+        clearing tables after tests so it won't cause a problem later
+         */
+        String url = "jdbc:postgresql://localhost:5432/codecoolshop";
+        String username = "postgres";
+        String password = "postgres";
 
+        Connection connection = DriverManager.getConnection(url, username, password);
+        Statement stmt = connection.createStatement();
+
+        // Use TRUNCATE
+        String sql = "TRUNCATE products, product_categories, suppliers";
+        // Execute deletion
+        stmt.executeUpdate(sql);
     }
 
     @Test
