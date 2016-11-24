6001a4bfb237720532793623c8a93cb8b9d8c0c4
ppayter-cc <pomsarpeter@gmail.com>
2016-11-24 17:44:11 +0100

    ProductCategoryDaoJdbcTest skeleton created

---
+++ test/com/codecool/shop/dao/implementation/database/ProductCategoryDaoJdbcTest.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.dao.implementation.database;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductCategoryDaoJdbcTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void add() throws Exception {
+
+    }
+
+    @Test
+    public void find() throws Exception {
+
+    }
+
+    @Test
+    public void remove() throws Exception {
+
+    }
+
+    @Test
+    public void getAll() throws Exception {
+
+    }
+
+}
