d0208f37b474fff0fa0309a5b417073484546fe7
Komlancz Janos <jkomlancz@gmail.com>
2016-12-08 15:39:18 +0100

    create orders_switch table

--- src/main/resources/sql/init_db.sql
+++ src/main/resources/sql/init_db.sql
@@ -1,8 +1,9 @@
--- DROP TABLE IF EXISTS products;
--- DROP TABLE IF EXISTS product_categories;
--- DROP TABLE IF EXISTS suppliers;
--- DROP TABLE IF EXISTS users;
--- DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS suppliers;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS orders_switch;
 
 CREATE TABLE product_categories
 (
@@ -50,4 +51,15 @@ shipping_address VARCHAR,
 total_price FLOAT,
 date_time VARCHAR,
 FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE orders_switch
+(
+id SERIAL PRIMARY KEY,
+order_id INTEGER,
+product_id INTEGER ,
+price FLOAT ,
+quantity INTEGER ,
+FOREIGN KEY (order_id) REFERENCES orders(id),
+FOREIGN KEY (product_id) REFERENCES products(id)
 );
